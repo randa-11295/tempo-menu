7443317b556dc8c57fe0a68a8867e9e00d508c3e
randa.ibrahim <randa.ibrahim@cartologic.com>
2024-01-06 10:47:03 +0200

    add main theme

---
+++ asssets/js/data.js
@@ -0,0 +1,357 @@
+const data = [
+  {
+    categoryName: "HOT COFFEE",
+    drinks: [
+      {
+        name: "ESPRESSO",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "TURKISH COFFEE",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "MACCHIATO",
+        sizes: [
+          { size: "SMALL", price: "45" },
+          { size: "Double", price: "55" },
+        ],
+      },
+      {
+        name: "AMERICANO",
+        sizes: [
+          { size: "SMALL", price: "$12" },
+          { size: "MEDIUM", price: "60" },
+          { size: "LARGE", price: "$12" },
+        ],
+      },
+      {
+        name: "LATTE",
+        sizes: [
+          { size: "SMALL", price: "$12" },
+          { size: "MEDIUM", price: "60" },
+          { size: "LARGE", price: "$12" },
+        ],
+      },
+      {
+        name: "CAPPUCCINO",
+        sizes: [
+          { size: "SMALL", price: "$12" },
+          { size: "MEDIUM", price: "60" },
+          { size: "LARGE", price: "$12" },
+        ],
+      },
+      {
+        name: "MOCHA",
+        sizes: [
+          { size: "SMALL", price: "$12" },
+          { size: "MEDIUM", price: "60" },
+          { size: "LARGE", price: "$12" },
+        ],
+      },
+      {
+        name: "WHITE MOCHA",
+        sizes: [
+          { size: "SMALL", price: "$12" },
+          { size: "MEDIUM", price: "60" },
+          { size: "LARGE", price: "$12" },
+        ],
+      },
+      {
+        name: "FLAT WHITE",
+        sizes: [
+          { size: "SMALL", price: "$12" },
+          { size: "MEDIUM", price: "60" },
+          { size: "LARGE", price: "$12" },
+        ],
+      },
+      {
+        name: "CORTADO",
+        sizes: [
+          { size: "SMALL", price: "$12" },
+          { size: "MEDIUM", price: "60" },
+          { size: "LARGE", price: "$12" },
+        ],
+      },
+      {
+        name: "AFFOGATO",
+        sizes: [
+          { size: "SMALL", price: "$12" },
+          { size: "MEDIUM", price: "60" },
+          { size: "LARGE", price: "$12" },
+        ],
+      },
+      {
+        name: "MATCHA LATTE",
+        sizes: [
+          { size: "SMALL", price: "$12" },
+          { size: "MEDIUM", price: "60" },
+          { size: "LARGE", price: "$12" },
+        ],
+      },
+      {
+        name: "PISTACHIO LATTE",
+        sizes: [
+          { size: "SMALL", price: "$12" },
+          { size: "MEDIUM", price: "60" },
+          { size: "LARGE", price: "$12" },
+        ],
+      },
+      {
+        name: "SPANISH LATTE",
+        sizes: [
+          { size: "SMALL", price: "$12" },
+          { size: "MEDIUM", price: "60" },
+          { size: "LARGE", price: "$12" },
+        ],
+      },
+      {
+        name: "SALTED CARAMEL LATTE",
+        sizes: [
+          { size: "SMALL", price: "$12" },
+          { size: "MEDIUM", price: "60" },
+          { size: "LARGE", price: "$12" },
+        ],
+      },
+      {
+        name: "CINNAMON CARAMEL LATTE",
+        sizes: [
+          { size: "SMALL", price: "$12" },
+          { size: "MEDIUM", price: "60" },
+          { size: "LARGE", price: "$12" },
+        ],
+      },
+      {
+        name: "CARAMEL MACCHIATO",
+        sizes: [
+          { size: "SMALL", price: "$12" },
+          { size: "MEDIUM", price: "60" },
+          { size: "LARGE", price: "$12" },
+        ],
+      },
+      {
+        name: "SYMPHONY",
+        sizes: [
+          { size: "SMALL", price: "$12" },
+          { size: "MEDIUM", price: "60" },
+          { size: "LARGE", price: "$12" },
+        ],
+      },
+      {
+        name: "AFFOGATO",
+        sizes: [
+          { size: "SMALL", price: "$12" },
+          { size: "MEDIUM", price: "60" },
+          { size: "LARGE", price: "$12" },
+        ],
+      },
+    ],
+  },
+
+  {
+    categoryName: "DRIP COFFEE",
+    drinks: [
+      {
+        name: "CHEMEX",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "V60",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "AEROPRESS",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "FRENCHPRESS",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "SYPHON",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+    ],
+  },
+  {
+    categoryName: "COLD BREW",
+    drinks: [
+      {
+        name: "PLAIN",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "SPANISH",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "PISTACHIO",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+    ],
+  },
+  {
+    categoryName: "COFFEE FRAPPE",
+    drinks: [
+      {
+        name: "COFFEE FRAPPE",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "CARAMEL COFFEE FRAPPE",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "MOCHA FRAPPE",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "WHITE MOCHA FRAPPE",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "TOFFEE CRUNCH FRAPPE",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+    ],
+  },
+  {
+    categoryName: "FREDDO",
+    drinks: [
+      {
+        name: "ESPRESSO FREDDO",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "LATTE FREDDO",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "CAPPUCCINO FREDDO",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "SPANISH FREDDO",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "LATTE FREDDO",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "TEMPO FREDDO",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+    ],
+  },
+  {
+    categoryName: "NON-COFFEE FRAPPE",
+    drinks: [
+      {
+        name: "CARAMEL FRAPPE",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "CHOCOLATE FRAPPE ",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "COTTON CANDY FRAPPE",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "BLUEBERRY FRAPPE",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "STRAWBERRY FRAPPE",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+      {
+        name: "ORIO FRAPPE",
+        sizes: [
+          { size: "SMALL", price: "40" },
+          { size: "Double", price: "50" },
+        ],
+      },
+    ],
+  },
+];
---
+++ asssets/js/tailwind.config.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+  theme: {
+      extend: {
+          colors: {
+              mainGreen: "#259448",
+              coffee: "#a29365",
+              petrol: "#34817b",
+              lightGrey: "#efeee5",
+              offWhite: "#fbf7eb",
+              orange: "#f3a220",
+          },
+          height: {
+              "40vh": "40vh",
+              "70vh": "70vh",
+          },
+          fontSize: {
+              "2rem": "2rem",
+          },
+          backgroundSize: { fill: "100% 100%" },
+      },
+  },
+};
