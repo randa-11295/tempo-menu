c353a03a6356dbf74257eb2520db77f9de612ad2
randa.ibrahim <randa.ibrahim@cartologic.com>
2024-01-06 16:13:27 +0200

    add aos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
+AOS.init();
+
 const createElHandle = (element, className) => {
   const box = document.createElement("section");
-  box.classList.add("w-11/12", "md:w-1/2", "xl:w-2/6", "px-2", "md:px-4", "py-4" ,"mx-auto");
+  box.classList.add(
+    "w-11/12",
+    "md:w-1/2",
+    "xl:w-2/6",
+    "px-2",
+    "md:px-4",
+    "py-4",
+    "mx-auto"
+  );
   box.innerHTML = element;
   document.querySelector(className).appendChild(box);
 };
@@ -8,11 +18,11 @@ const createElHandle = (element, className) => {
 // * custom components
 const categoriesComponent = (categoryEl) => `
 
-<h3 class="text-smoothGreen font-bold py-2 text-gl">${
+<h3  data-aos="zoom-in-up" data-aos-duration="3000" class="text-smoothGreen font-bold py-2 text-gl">${
   categoryEl?.categoryName || "drink"
 }</h3>
 ${categoryEl?.items?.map(
-  (itemEl) => `<div class="flex justify-between  py-1 text-sm">
+  (itemEl) => `<div  data-aos="zoom-in-up" data-aos-duration="3000" class="flex justify-between  py-1 text-sm">
     
         <p>${itemEl.itemName}</p>
 
