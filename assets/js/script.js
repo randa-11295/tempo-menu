dd0470518491a4c80cb563882bab8a97fddc9fcd
randa.ibrahim <randa.ibrahim@cartologic.com>
2024-01-06 14:23:52 +0200

    update responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 const createElHandle = (element, className) => {
-  const box = document.createElement("div");
+  const box = document.createElement("section");
+  box.classList.add("w-11/12", "md:w-1/2", "xl:w-2/6", "px-2", "md:px-4", "py-4" ,"mx-auto");
   box.innerHTML = element;
   document.querySelector(className).appendChild(box);
 };
 
 // * custom components
 const categoriesComponent = (categoryEl) => `
-<section class=" w-[100%] px-4 pt-2">
 
 <h3 class="text-smoothGreen font-bold py-2 text-gl">${
   categoryEl?.categoryName || "drink"
@@ -19,16 +19,14 @@ ${categoryEl?.items?.map(
         <aside class="flex">
         ${itemEl?.sizes?.map(
           (sizeEl) => ` 
-            <figure class="pl-3">
+            <figure class="pr-2">
                 <b  class="text-smoothGreen">${sizeEl?.sizeName}</b>
                 <span>20</span>
             </figure>  `
-            )}
+        )}
             </aside>
     </div>`
 )}
-
-</section>
 `;
 
 data.forEach((el) => {
